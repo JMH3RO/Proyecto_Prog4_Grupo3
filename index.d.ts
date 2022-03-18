475f681aff2c864ba49150000393a78d9e691743
JMH3RO <98042723+JMH3RO@users.noreply.github.com>
2022-03-17 20:02:44 -0600

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, AppresApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<AppresApplication>;
