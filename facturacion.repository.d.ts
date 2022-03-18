4366db2ef2f23988fe9df64a447d6359a3b363fb
JMH3RO <98042723+JMH3RO@users.noreply.github.com>
2022-03-17 20:18:54 -0600

    Delete facturacion.repository.d.ts

--- facturacion.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { ConnDataSource } from '../datasources';
-import { Facturacion, FacturacionRelations } from '../models';
-export declare class FacturacionRepository extends DefaultCrudRepository<Facturacion, typeof Facturacion.prototype.idfactura, FacturacionRelations> {
-    constructor(dataSource: ConnDataSource);
-}
