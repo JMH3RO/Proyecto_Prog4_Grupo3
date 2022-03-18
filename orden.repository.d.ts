1d610c0bc1d53e57d1d5de082a12e9f686593b54
JMH3RO <98042723+JMH3RO@users.noreply.github.com>
2022-03-17 20:25:07 -0600

    Delete orden.repository.d.ts

--- orden.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { ConnDataSource } from '../datasources';
-import { Orden, OrdenRelations } from '../models';
-export declare class OrdenRepository extends DefaultCrudRepository<Orden, typeof Orden.prototype.idorden, OrdenRelations> {
-    constructor(dataSource: ConnDataSource);
-}
