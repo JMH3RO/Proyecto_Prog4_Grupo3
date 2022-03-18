5354f0acc5679321973bed4678378ff919f89f46
JMH3RO <98042723+JMH3RO@users.noreply.github.com>
2022-03-17 20:23:01 -0600

    Delete producto.repository.d.ts

--- producto.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { ConnDataSource } from '../datasources';
-import { Producto, ProductoRelations } from '../models';
-export declare class ProductoRepository extends DefaultCrudRepository<Producto, typeof Producto.prototype.idproducto, ProductoRelations> {
-    constructor(dataSource: ConnDataSource);
-}
