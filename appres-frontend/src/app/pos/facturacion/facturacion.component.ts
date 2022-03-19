12843d389226e0cd93bcdf9619e1c9189c1eb964
mhayde <mhayde@unitec.edu>
2022-03-18 18:30:10 -0600

    adding facturacion

--- appres-frontend/src/app/pos/facturacion/facturacion.component.ts
+++ appres-frontend/src/app/pos/facturacion/facturacion.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { Facturacion } from 'src/app/api/models';
+import { FacturacionControllerService } from 'src/app/api/services';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms'
 
 @Component({
   selector: 'app-facturacion',
@@ -7,9 +12,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacturacionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,//Inyeccion de la herramienta para construir formularios
+    private FacturacionServices:FacturacionControllerService
+  ) {
+    }
 
-  ngOnInit(): void {
+//Creo(FormGroup) y defino estructura de un formulario(FormBuilder)
+  formFacturacion: FormGroup  =   this.fb.group({
+    idfacturacion:[],
+    idorden:[,[Validators.required]],
+    total:[,[Validators.required]],
+    metodopago:[,[Validators.required]],
+    
+  })
+;
+
+visible:boolean=false
+facturacion:Facturacion[]=[]; //Arreglo de datos de productos
+
+  open(data?:Facturacion): void {
+    if(data)
+      this.formFacturacion.setValue(data)
+    this.visible = true;
   }
 
+  close(): void {
+    this.visible = false;
+    this.formFacturacion.reset()
+  }
+
+  ngOnInit(): void {
+    this.getFacturas()
+  }
+
+  getFacturas():void{
+    this.FacturacionServices.find().subscribe(datos => this.facturacion = datos )
+  }
+
+  guardar():void{
+    if(this.formFacturacion.value.idproducto){//Actualizar si existe id
+      this.FacturacionServices.updateById({'id':this.formFacturacion.value.idproducto,'body':this.formFacturacion.value}).subscribe(
+        () => 
+        {
+          this.facturacion = this.facturacion.map(obj => {
+            if (obj.idfactura === this.formFacturacion.value.idproducto)
+              return this.formFacturacion.value;          
+            return obj;
+          });
+
+          this.formFacturacion.reset()
+          this.close();
+        }
+      )
+    }else{//crea uno nuevo si no existe
+    delete this.formFacturacion.value.idproducto
+    this.FacturacionServices.create({'body':this.formFacturacion.value}).subscribe(
+      datoAgregado=>
+      {
+        this.facturacion = [...this.facturacion,datoAgregado]
+        this.formFacturacion.reset()
+        this.close();
+      }
+      )
+    }
+  }
+
+  delete(id:any):void{
+    this.FacturacionServices.deleteById({id}).subscribe(() => {
+      this.facturacion = this.facturacion.filter(x => x.idfactura !== id);
+    })
+  }
 }
--- appres-frontend/src/app/pos/pos.module.ts
+++ appres-frontend/src/app/pos/pos.module.ts
@@ -4,6 +4,17 @@ import { CommonModule } from '@angular/common';
 import { POSRoutingModule } from './pos-routing.module';
 import { FacturacionComponent } from './facturacion/facturacion.component';
 
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import {FormBuilder,FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +22,18 @@ import { FacturacionComponent } from './facturacion/facturacion.component';
   ],
   imports: [
     CommonModule,
-    POSRoutingModule
+    POSRoutingModule,
+    NzDividerModule,
+    NzTableModule,
+    FormsModule,
+    NzFormModule,
+    NzDatePickerModule,
+    NzDrawerModule,
+    NzInputModule,
+    NzSelectModule,
+    NzInputNumberModule,
+    NzButtonModule,
+    ReactiveFormsModule,
   ]
 })
 export class POSModule { }
