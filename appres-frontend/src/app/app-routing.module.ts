4e31c8602f7fafbcf841454200b69337b50ddfe0
mhayde <mhayde@unitec.edu>
2022-03-26 11:55:45 -0600

    adding loging module

--- appres-frontend/src/app/app-routing.module.ts
+++ appres-frontend/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '' },
+  { path: '', pathMatch: 'full', redirectTo: 'acceso/login' },
   { path: 'menus', loadChildren: () => import('./menus/menus.module').then(m => m.MenusModule) },
   { path: 'pos', loadChildren: () => import('./pos/pos.module').then(m => m.POSModule) },
+  { path: 'acceso', loadChildren: () => import('./acceso/acceso.module').then(m => m.AccesoModule) }
 ];
 
 @NgModule({
