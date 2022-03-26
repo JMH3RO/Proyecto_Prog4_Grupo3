b9e7e83978649377b82d70f31b2cf0424604079b
mhayde <mhayde@unitec.edu>
2022-03-26 14:41:37 -0600

    adding the form

--- appres-frontend/src/app/acceso/acceso-routing.module.ts
+++ appres-frontend/src/app/acceso/acceso-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {FormBuilder,FormGroup,ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path:'',
--- appres-frontend/src/app/acceso/acceso.module.ts
+++ appres-frontend/src/app/acceso/acceso.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NzFormModule } from 'ng-zorro-antd/form';
 import { AccesoRoutingModule } from './acceso-routing.module';
 import { LoginComponent } from './login/login.component';
-
-
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
     CommonModule,
-    AccesoRoutingModule
+    AccesoRoutingModule,
+    NzFormModule,
+    NzInputModule,
+    FormBuilder,
+    FormGroup,
+    Validators,
+    ReactiveFormsModule,
+    NzButtonModule
   ]
 })
 export class AccesoModule { }
